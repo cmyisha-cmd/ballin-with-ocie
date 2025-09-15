e2c755a65d736a88c3e5274d99aade8eba0fc45a
cmyisha-cmd <c.myisha@gmail.com>
2025-09-15 16:39:01 -0400

    Delete frontend directory

--- frontend/src/pages/Home.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-export default function Home() {
-  return (
-    <div style={{padding:"2rem", textAlign:"center"}}>
-      <h1 style={{color:"#8A2BE2", fontSize:"3rem"}}>Welcome to Ballin' with Ocie!</h1>
-      <p>Register for contests, get tickets, and leave birthday wishes.</p>
-    </div>
-  );
-}
